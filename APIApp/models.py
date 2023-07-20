1d63c17cd1ab84f7698cbd6c2392760a8b60da2a
Sumon Ahmed <bdsumon4u@gmail.com>
2023-07-20 14:50:39 +0600

    TypoFix

--- APIApp/models.py
+++ APIApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey('auth.User', related_name='products', on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
     price = models.FloatField()
     description = models.TextField()
